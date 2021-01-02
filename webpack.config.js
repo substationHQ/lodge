d58da03f45502993de2455c4caa5868b02b5d608
Alan Mooiman <amooiman@gmail.com>
2021-01-02 10:53:07 -0500

    Bundle CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const path = require("path");
 
 module.exports = ["source-map"].map((devtool) => ({
   mode: "production",
-  entry: "./source/index.js",
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  entry: {
+    lodge: "./source/index.js",
+    overlay: "./source/templates/overlay.css",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "lodge.js",
+    filename: "[name].js",
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
   },
   devtool,
 }));
